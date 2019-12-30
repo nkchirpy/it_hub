009175518886014ea953e3412376f303436e5118
nandakrishnan <nandakrishnan@pinnacleseven.com>
2019-12-30 23:20:26 +0530

    models created

--- sales/employee/admin.py
+++ sales/employee/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CallAllocation, Customer, Engineer
 
 # Register your models here.
+
+
+admin.site.register(CallAllocation)
+admin.site.register(Customer)
+admin.site.register(Engineer)
--- sales/employee/models.py
+++ sales/employee/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+
+
+class CallAllocation(models.Model):
+
+
+    PRIORITY_CHOICES = (
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    )
+    
+
+    hub_status = (
+        ('not_yet', 'Not Yet'),
+        ('in_progress', 'In Progress'),
+        ('on_hold', 'On hold'),
+        ('completed', 'Completed'),
+
+    )
+    
+    name = models.CharField(max_length=200)
+    customer = models.ForeignKey(
+        'Customer',
+        on_delete=models.CASCADE,
+    )
+    product = models.CharField(max_length=200)
+    s_no = models.IntegerField()
+    issue = models.CharField(max_length=200)
+    priority = models.CharField(max_length=10,choices=PRIORITY_CHOICES)
+    estimate_hours = models.FloatField()
+    engineer = models.ForeignKey(
+        'Engineer',
+        on_delete=models.CASCADE,
+    )
+
+    status = models.CharField(max_length=15,choices=hub_status)
+    start_date = models.DateTimeField(default=datetime.now)
+    end_date = models.DateTimeField()
+    remarks = models.CharField( max_length=2000)
+
+
+    def __str__(self):
+        return self.engineer
+ 
+
+class Customer(models.Model):
+
+    name = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.name
+
+
+
+class Engineer(models.Model):
+
+    name = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.name
+
+
+
+
